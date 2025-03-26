5bd52e0424e2e1bb0d717ced56978e7a34864a94
MotherStarry <darkxwolf17@yahoo.com>
2025-03-26 16:57:17 +0100

    Initial commit

---
+++ src/main/java/net/starry/paradox/ParadoxClient.java
@@ -0,0 +1,9 @@
+package net.starry.paradox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ParadoxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/starry/paradox/ParadoxDataGenerator.java
@@ -0,0 +1,11 @@
+package net.starry.paradox;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ParadoxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
